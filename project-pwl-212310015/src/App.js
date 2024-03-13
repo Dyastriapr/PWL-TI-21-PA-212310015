4f3edd639608d52a4bb2f3f9aba776112edcc21f
Dyas Tri <dyastriap12@gmail.com>
2024-03-14 00:09:48 +0700

    tugas satu

---
+++ project-pwl-212310015/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Biodata from "./component/Biodata";
+
+function App() {
+  return (
+    <div>
+      <Biodata />
+    </div>
+  );
+}
+
+export default App;
