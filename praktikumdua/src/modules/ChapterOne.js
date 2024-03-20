d0e70fb1aab565cd1bd909f04e2942b4b0356819
Dyastriapr <dyastriap1@gmail.com>
2024-03-21 00:57:57 +0700

    tugas dua asli

---
+++ praktikumdua/src/modules/ChapterOne.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class ChapterOne extends Component {
+  render() {
+    return (
+      <div className="container-fluid mx-auto">
+        <h1 className="text-center ">Chapter One: The Beginning</h1>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+        <p>INI NANTI TAMPILAN CHAT</p>
+      </div>
+    );
+  }
+}
